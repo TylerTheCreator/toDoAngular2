06d2be8e55d3d2b19583f4b35b5d6ce50d6dcca0
matthew newell <mnewell10@gmail.com>
2016-03-22 13:23:41 -0700

    Implement  checkboxes

---
+++ app/done.pipe.ts
@@ -0,0 +1,28 @@
+import {Pipe, PipeTransform} from 'angular2/core';
+import {Task} from './task.model';
+
+@Pipe({
+  name: "done", //refer to the pipe
+  pure: false
+})
+export class DonePipe implements PipeTransform {
+  transform(input: Task[], args) {
+    var desiredDoneState = args[0];
+    if(desiredDoneState === "done") {
+      return input.filter((task) => {
+        return task.done;
+      });
+    }else if (desiredDoneState === "notDone") {
+      return input.filter((task) => {
+        return !task.done;
+      });
+    } else {
+      return input;
+    }
+  }
+}
+// export class DonePipe implements PipeTransform {
+//   transform(input: Task[], args) {
+//     console.log('selected task: ', args[1]);
+//   }
+// }
---
+++ app/task.component.ts
@@ -0,0 +1,14 @@
+import { Component } from 'angular2/core';
+import { Task } from './task.model';
+
+@Component({
+    selector: 'task-display',
+    inputs: ['task'],
+  templateUrl: 'app/task.component.html' 
+})
+export class TaskComponent {
+  public task: Task;
+  toggleDone(setState: boolean){
+    this.task.done = setState;
+  }
+}
