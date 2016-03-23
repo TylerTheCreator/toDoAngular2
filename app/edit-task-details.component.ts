31c6382813dce354d5ab7ec68adef81e47d2c318
Matthew Newell <mnewell10@gmail.com>
2016-03-23 16:49:44 -0700

    Add some important notes to edit task details

--- app/edit-task-details.component.ts
+++ app/edit-task-details.component.ts
@@ -4,7 +4,10 @@ import {Task} from './task.model';
 @Component({
   selector: 'edit-task-details',
   inputs: ['task'],
-  template: `
+  template:
+// TWO WAY DATABINDING! ngModel special directive
+// When we display the description, data is just moving out, from the model to the view. We want to go the other way too - we want to enter a new description into the view using a form and have it travel back through the controller to the model and change the value of the property. We can do that with a special directive called [(ngModel)].
+  `
     <div class="task-form">
       <h3>Edit Description: </h3>
       <input [(ngModel)]="task.description" class="col-sm-8 input-lg task-form"/>
